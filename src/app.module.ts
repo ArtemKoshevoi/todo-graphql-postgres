f4988f5f306e7040f56c891695f3e15983ab0974
2mcKosh <2mcKosh@MacBook-Pro.local>
2022-08-01 14:18:08 +0300

    add initials configs

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    TypeOrmModule.forRoot({
+      keepConnectionAlive: true,
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      // entities: [User, Profile],
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TasksModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
